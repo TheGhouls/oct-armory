81e0ef2fec152a76dbf0e5ddbab58ebb7942b51e
aurel <aurel.pigot@gmail.com>
2016-06-05 01:17:52 +0200

    first commit

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,6 +1,6 @@
 import {getRepoReadme, getReposArmory, getUserRepo} from '../../api/github/methods.js';
 import {addPlan} from '../../api/plans/plansMethods.es6.js';
-import { PlansIndex } from '../../api/serarch/server/defineSearchSource.js';
+import { PlansIndex } from '../../api/search/server/defineSearchSource.js';
 import '../../api/users/server/publish.js';
 import "../../api/plans/server/publish.js";
 //import { log } from '../../api/logger_conf.js';
